64ab3d9974c912da5fa0021def4580fb65a96597
Halano <70325026+HalanoSiblee@users.noreply.github.com>
2022-12-06 09:26:44 +0200

    Add files via upload
    Publicip is simple batch script to show & save your public ip

---
+++ publicip.bat
@@ -0,0 +1,25 @@
+@echo off
+if "%1"=="save" (goto save)
+if "%1"=="list" (goto list)
+if "%1"=="clip" (goto clip)
+echo:
+echo -------------------------
+curl ident.me
+echo: 
+echo -------------------------
+echo:
+echo Try publicip clip to save it into clipboard
+pause
+exit /b
+:save
+echo: >> Lastip.dat
+curl ident.me >> Lastip.dat
+echo Public Ip saved.
+exit /b
+:clip
+curl ident.me |clip
+exit /b
+:list
+type lastip.dat
+echo:
+echo -------------------------------------------------------
